16868c047b3aaf04a467bd978aa70fa6e15b73d6
duxuhao <duxuhao88@gmail.com>
2016-04-02 02:25:03 +0800

    some chart

--- RankingClassification.R
+++ RankingClassification.R
@@ -1,8 +1,8 @@
 library(caret)
 library(plotly)
 
-Dataset <- read.csv("UniversityDataPreviousYearPrediction.csv")
-Dataset <- Dataset[!is.na(Dataset$Lowest_Ranking),]
+Dataset <- read.csv("UniversityDataUsed.csv")
+#Dataset <- Dataset[!is.na(Dataset$Lowest_Ranking),]
 Dataset[,7] <- as.numeric(Dataset[,7])
 
 
@@ -11,9 +11,18 @@ Year <- unique(Dataset["Year"])
 University <- unique(Dataset["University_Name"])
 Topic <- unique(Dataset["Topic"])
 
-DF <- Dataset[,c("Topic","Province","Year","Plan_Number")]
+DF <- Dataset[,c("Province","Year","Plan_Number")]
 
-PlanProvince <- aggregate(DF$Plan_Number,by=list(DF$Topic,DF$Province,DF$Year),FUN=sum)
+PlanProvince <- aggregate(DF$Plan_Number,by=list(DF$Province,DF$Year),FUN=sum)
+colnames(PlanProvince) <- c("Province","Year","Planned_Quantity_for_enrolling")
+
+NoGD <- PlanProvince[PlanProvince$Province!=PlanProvince$Province[10],]
+ggplot(NoGD,aes(x=Province,y=Planned_Quantity_for_enrolling,fill=as.factor(Year),width=0.8)) + geom_bar(stat="identity", position="dodge")
+
+GD <- PlanProvince[PlanProvince$Province==PlanProvince$Province[10],]
+ggplot(GD,aes(x=Year,y=Planned_Quantity_for_enrolling)) + geom_bar(stat="identity")
+
+Eco <- unique(cbind(Dataset[,c("Year","Province")],Dataset$GDP / Dataset$Population))
 
 
 if(FALSE) {
