b9e20d4cb1ef676824b04ec6019e23075233e1f7
duxuhao <duxuhao88@gmail.com>
2016-04-09 16:08:19 +0800

    Nan

--- Universitycluster.py
+++ Universitycluster.py
@@ -1,8 +1,13 @@
 import pandas as pd
 from sklearn import cluster
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv("UniversityData.csv") #kind of origin data
-a = df[(df.Year == 2010) * (df.Topic == "理科")]
-X = a[["UniversityNo","Ranking_Scores","Distance","Plan_Number","Lowest_Ranking"]]
-X = a[["Ranking_Percentage","Lowest_Ranking"]]
+#df = df.sort(['Score'],ascending = 0)
+df.Label = 0
+T = df[(df.Year == 2015) & (df.Topic == "理科")].Score
+boundary = np.max(T.values)
+boundary = boundary - (750 - bounrady) * 0.1
+df.loc[(df.Year == 2015) & (df.Topic == "理科") & (df.Score < boundary),'Label']  += 1
+boundary = np.max(T.values[T.values < boundary])
